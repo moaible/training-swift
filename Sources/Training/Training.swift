777dc18fafb783bf1c929e6e83c233fba679a4cc
Hiromi Motodera <moai.motodera@gmail.com>
2021-11-15 05:23:39 +0000

    Add swift package initialized files

---
+++ Sources/Training/Training.swift
@@ -0,0 +1,6 @@
+public struct Training {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/TrainingTests/TrainingTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Training
+
+final class TrainingTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Training().text, "Hello, World!")
+    }
+}
