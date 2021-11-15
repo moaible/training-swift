e6fb324fd7cfdd615752b5e7098a0a831ce0483e
Hiromi Motodera <moai.motodera@gmail.com>
2021-11-15 09:16:24 +0000

    Refactor items

--- Sources/Training/Training.swift
+++ Sources/Training/Training.swift
@@ -1,6 +1,5 @@
 
 public protocol TrainingItemConvertible {
-    var question: String { get }
     associatedtype Input
     associatedtype Output
     func solve(_ input: Input) -> Output
@@ -8,150 +7,72 @@ public protocol TrainingItemConvertible {
 
 public class Quiz000: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        ---
-        実行するとHello World!と表示するプログラムを作成せよ。
-        【実行例】
-        Hello World!
-        ---
-        """
-    }
-
     public typealias Input = Void
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let answer = "Hello World!"
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz001: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        ---
-        12345+23456を計算して結果を表示するプログラムを作成せよ。
-        【実行例】
-        12345 + 23456 = 35801
-        ---
-        """
-    }
-
     public typealias Input = (first: Int, second: Int)
     public typealias Output = Int
     
     public func solve(_ input: Input) -> Output {
-        print(question)
         let answer = input.first + input.second
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz002: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        12345を7で割った余りを表示するプログラムを作成せよ。
-        【実行例】
-        12345 ÷ 7 の余りは 4
-        $
-        """
-    }
-
     public typealias Input = (number: Int, divisor: Int)
     public typealias Output = String
     
     public func solve(_ input: Input) -> Output {
-        print(question)
         let result = input.number % input.divisor
         let answer = "\(input.number) ÷ \(input.divisor) の余りは \(result)"
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz003: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、その入力値を表示するプログラムを作成せよ。
-        【実行例、下線部は入力例】
-        input number: 123
-        your number is 123
-        ---
-        """
-    }
-
     public typealias Input = Int
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let answer = "your number is \(input)"
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz004: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、その入力値を3倍した計算結果を表示するプログラムを作成せよ。
-        【実行例、下線部は入力例】
-        input number: 123
-        answer = 369
-        ---
-        """
-    }
-
     public typealias Input = Int
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let result = input * 3
         let answer = "answer = \(result)"
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz005: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        ---
-        整数値を2つ入力させ、それらの値の和、差、積、商と余りを求めるプログラムを作成せよ。なお、差と商は1つ目の値から2つ目の値を引いた、あるいは割った結果とする。余りは無い場合も0と表示するのでよい。
-        【実行例、下線部は入力例】
-        input 1st number: 123
-        input 2nd number: 7
-        和: 130
-        差: 116
-        積: 861
-        商: 17, 余り: 4
-        input 1st number: 123
-        input 2nd number: 3
-        和: 126
-        差: 120
-        積: 369
-        商: 41, 余り: 0
-        ---
-        """
-    }
-
     public typealias Input = (first: Int, second: Int)
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let summation = input.first + input.second
         let difference = input.first - input.second
         let product = input.first * input.second
@@ -163,95 +84,54 @@ public class Quiz005: TrainingItemConvertible {
         積: \(product)
         商: \(quotient), 余り: \(remainder)
         """
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz006: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、値が0ならzeroと表示するプログラムを作成せよ。
-        【実行例、下線部は入力例】
-        input number: 0
-        zero
-        input number: 1
-        ---
-        """
-    }
-
     public typealias Input = Int
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         var answer = ""
         if input == 0 {
             answer = "zero"
         }
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz007: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、値が0ならzero、0でなければnot zeroと表示するプログラムを作成せよ。
-        【実行例、下線部は入力例】
-        input number: 0
-        zero
-        input number: 1
-        not zero
-        ---
-        """
-    }
-
     public typealias Input = Int
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let answer: String
         if input == 0 {
             answer = "zero"
         } else {
             answer = "not zero"
         }
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz008: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、値が正であればpositiveと表示するプログラムを作成せよ。ただし0は正には含まない。
-        【実行例、下線部は入力例】
-        input number: 1
-        positive
-        input number: -1
-        input number: 0
-        ---
-        """
-    }
-
     public typealias Input = Int
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         var answer = ""
         if input > 0 {
             answer = "positive"
         }
-        print(answer)
+        
         return answer
     }
 }
@@ -275,26 +155,10 @@ public class Quiz009: TrainingItemConvertible {
         }
     }
 
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、値が正であればpositive、負であればnegative、0であればzeroと表示するプログラムを作成せよ。
-        【実行例、下線部は入力例】
-        input number: 1
-        positive
-        input number: -1
-        negative
-        input number: 0
-        zero
-        ---
-        """
-    }
-
     public typealias Input = Int
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let answer: Result
         if input == 0 {
             answer = .zero
@@ -303,210 +167,95 @@ public class Quiz009: TrainingItemConvertible {
         } else {
             answer = .negative
         }
-        print(answer)
+        
         return answer.identifier()
     }
 }
 
 public class Quiz010: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、その値を絶対値にして表示するプログラムを作成せよ。（できれば変数の値を絶対値に変えるようにせよ）
-        【実行例、下線部は入力例】
-        input number: 1
-        absolute value is 1
-        input number: -2
-        absolute value is 2
-        ---
-        """
-    }
-
     public typealias Input = Int
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let answer = "absolute value is \(abs(input))"
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz011: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        ---
-        Hello World!を10回繰り返して表示するプログラムを作成せよ。
-        【実行例、下線部は入力例】
-        Hello World!
-        Hello World!
-        Hello World!
-        Hello World!
-        Hello World!
-        Hello World!
-        Hello World!
-        Hello World!
-        Hello World!
-        Hello World!
-        ---
-        """
-    }
-
     public typealias Input = Void
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let answer = (0 ..< 10)
             .map { _ in "Hello World!" }
             .joined(separator: "\n")
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz012: TrainingItemConvertible {
 
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、その値の回数だけHello World!を繰り返して表示するプログラムを作成せよ。
-        【実行例、下線部は入力例】
-        input number: 7
-        Hello World!
-        Hello World!
-        Hello World!
-        Hello World!
-        Hello World!
-        Hello World!
-        Hello World!
-        ---
-        """
-    }
-
     public typealias Input = Int
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let answer = (0 ..< input)
             .map { _ in "Hello World!" }
             .joined(separator: "\n")
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz013: TrainingItemConvertible {
-    
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、0から入力値まで数を1ずつ増やして表示するプログラムを作成せよ。
-
-        【実行例、下線部は入力例】
-        input number: 5
-        0
-        1
-        2
-        3
-        4
-        5
-        ---
-        """
-    }
 
     public typealias Input = Int
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let answer = (0 ... input)
             .map { idx in String(idx) }
             .joined(separator: "\n")
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz014: TrainingItemConvertible {
-    
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、入力値から0まで数を1ずつ減らして表示するプログラムを作成せよ。
-        【実行例、下線部は入力例】
-        input number: 5
-        5
-        4
-        3
-        2
-        1
-        0
-        ---
-        """
-    }
 
     public typealias Input = Int
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let answer = (0 ... input)
             .reversed()
             .map { idx in String(idx) }
             .joined(separator: "\n")
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz015: TrainingItemConvertible {
-    
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、0から入力値を超えない値まで2ずつ増やして表示するプログラムを作成せよ。
-        【実行例、下線部は入力例】
-        input number: 7
-        0
-        2
-        4
-        6
-        ---
-        """
-    }
 
     public typealias Input = Int
     public typealias Output = String
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let answer = (0 ... input / 2)
             .map { idx in String(idx * 2) }
             .joined(separator: "\n")
-        print(answer)
+        
         return answer
     }
 }
 
 public class Quiz016: TrainingItemConvertible {
-    
-    public var question: String {
-        """
-        ---
-        整数値を入力させ、入力値が0でなければ再度入力させ、0であれば終了するプログラムを作成せよ。
-        【実行例、下線部は入力例】
-        input number: 5
-        input number: 3
-        input number: -4
-        input number: 0
-        ---
-        """
-    }
 
     public typealias Input = [Int]
     public typealias Output = [String]
@@ -529,12 +278,11 @@ public class Quiz016: TrainingItemConvertible {
     }
 
     public func solve(_ input: Input) -> Output {
-        print(question)
         let end = 0
         let answer = untilElement(input, atEnd: end).map {
             $0 == end ? "end" : "continued"
         }
-        print(answer)
+        
         return answer
     }
 }
--- Tests/TrainingTests/TrainingTests.swift
+++ Tests/TrainingTests/TrainingTests.swift
@@ -4,26 +4,71 @@ import XCTest
 final class TrainingTests: XCTestCase {
 
     func testQuiz000() throws {
+        print("""
+        実行するとHello World!と表示するプログラムを作成せよ。
+        【実行例】
+        Hello World!
+        """)
         XCTAssertEqual(Quiz000().solve(()), "Hello World!")
     }
 
     func testQuiz001() throws {
+        print("""
+        12345+23456を計算して結果を表示するプログラムを作成せよ。
+        【実行例】
+        12345 + 23456 = 35801
+        """)
         XCTAssertEqual(Quiz001().solve((12345, 23456)), 35801)
     }
 
     func testQuiz002() throws {
+        print("""
+        12345を7で割った余りを表示するプログラムを作成せよ。
+        【実行例】
+        12345 ÷ 7 の余りは 4
+        $
+        """)
         XCTAssertEqual(Quiz002().solve((12345, 7)), "12345 ÷ 7 の余りは 4")
     }
 
     func testQuiz003() throws {
+        print("""
+        整数値を入力させ、その入力値を表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 123
+        your number is 123
+        """)
         XCTAssertEqual(Quiz003().solve(123), "your number is 123")
     }
 
     func testQuiz004() throws {
+        print("""
+        整数値を入力させ、その入力値を3倍した計算結果を表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 123
+        answer = 369
+        """)
         XCTAssertEqual(Quiz004().solve(123), "answer = 369")
     }
 
     func testQuiz005() throws {
+        print("""
+        整数値を2つ入力させ、それらの値の和、差、積、商と余りを求めるプログラムを作成せよ。
+        なお、差と商は1つ目の値から2つ目の値を引いた、あるいは割った結果とする。余りは無い場合も0と表示するのでよい。
+        【実行例、下線部は入力例】
+        input 1st number: 123
+        input 2nd number: 7
+        和: 130
+        差: 116
+        積: 861
+        商: 17, 余り: 4
+        input 1st number: 123
+        input 2nd number: 3
+        和: 126
+        差: 120
+        積: 369
+        商: 41, 余り: 0
+        """)
         XCTAssertEqual(Quiz005().solve((123, 7)), """
         和: 130
         差: 116
@@ -39,33 +84,88 @@ final class TrainingTests: XCTestCase {
     }
 
     func testQuiz006() throws {
+        print("""
+        整数値を入力させ、値が0ならzeroと表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 0
+        zero
+        input number: 1
+        """)
         XCTAssertEqual(Quiz006().solve(0), "zero")
         XCTAssertEqual(Quiz006().solve(1), "")
     }
 
     func testQuiz007() throws {
+        print("""
+        整数値を入力させ、値が0ならzero、0でなければnot zeroと表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 0
+        zero
+        input number: 1
+        not zero
+        """)
         XCTAssertEqual(Quiz007().solve(0), "zero")
         XCTAssertEqual(Quiz007().solve(1), "not zero")
     }
 
     func testQuiz008() throws {
+        print("""
+        整数値を入力させ、値が正であればpositiveと表示するプログラムを作成せよ。ただし0は正には含まない。
+        【実行例、下線部は入力例】
+        input number: 1
+        positive
+        input number: -1
+        input number: 0
+        """)
         XCTAssertEqual(Quiz008().solve(0), "")
         XCTAssertEqual(Quiz008().solve(-1), "")
         XCTAssertEqual(Quiz008().solve(1), "positive")
     }
 
     func testQuiz009() throws {
+        print("""
+        整数値を入力させ、値が正であればpositive、負であればnegative、0であればzeroと表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 1
+        positive
+        input number: -1
+        negative
+        input number: 0
+        zero
+        """)
         XCTAssertEqual(Quiz009().solve(0), "zero")
         XCTAssertEqual(Quiz009().solve(-1), "negative")
         XCTAssertEqual(Quiz009().solve(1), "positive")
     }
 
     func testQuiz010() throws {
+        print("""
+        整数値を入力させ、その値を絶対値にして表示するプログラムを作成せよ。（できれば変数の値を絶対値に変えるようにせよ）
+        【実行例、下線部は入力例】
+        input number: 1
+        absolute value is 1
+        input number: -2
+        absolute value is 2
+        """)
         XCTAssertEqual(Quiz010().solve(1), "absolute value is 1")
         XCTAssertEqual(Quiz010().solve(-2), "absolute value is 2")
     }
 
     func testQuiz011() throws {
+        print("""
+        Hello World!を10回繰り返して表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        Hello World!
+        Hello World!
+        Hello World!
+        Hello World!
+        Hello World!
+        Hello World!
+        Hello World!
+        Hello World!
+        Hello World!
+        Hello World!
+        """)
         XCTAssertEqual(Quiz011().solve(()), """
         Hello World!
         Hello World!
@@ -81,6 +181,18 @@ final class TrainingTests: XCTestCase {
     }
 
     func testQuiz012() throws {
+        print("""
+        整数値を入力させ、その値の回数だけHello World!を繰り返して表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 7
+        Hello World!
+        Hello World!
+        Hello World!
+        Hello World!
+        Hello World!
+        Hello World!
+        Hello World!
+        """)
         XCTAssertEqual(Quiz012().solve(7), """
         Hello World!
         Hello World!
@@ -93,6 +205,17 @@ final class TrainingTests: XCTestCase {
     }
 
     func testQuiz013() throws {
+        print("""
+        整数値を入力させ、0から入力値まで数を1ずつ増やして表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 5
+        0
+        1
+        2
+        3
+        4
+        5
+        """)
         XCTAssertEqual(Quiz013().solve(5), """
         0
         1
@@ -104,6 +227,17 @@ final class TrainingTests: XCTestCase {
     }
 
     func testQuiz014() throws {
+        print("""
+        整数値を入力させ、入力値から0まで数を1ずつ減らして表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 5
+        5
+        4
+        3
+        2
+        1
+        0
+        """)
         XCTAssertEqual(Quiz014().solve(5), """
         5
         4
@@ -115,6 +249,15 @@ final class TrainingTests: XCTestCase {
     }
 
     func testQuiz015() throws {
+        print("""
+        整数値を入力させ、0から入力値を超えない値まで2ずつ増やして表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 7
+        0
+        2
+        4
+        6
+        """)
         XCTAssertEqual(Quiz015().solve(7), """
         0
         2
@@ -124,6 +267,14 @@ final class TrainingTests: XCTestCase {
     }
 
     func testQuiz016() throws {
+        print("""
+        整数値を入力させ、入力値が0でなければ再度入力させ、0であれば終了するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 5
+        input number: 3
+        input number: -4
+        input number: 0
+        """)
         XCTAssertEqual(
             Quiz016().solve([]), [])
         XCTAssertEqual(
