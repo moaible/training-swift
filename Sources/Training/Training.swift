0f224efaf7b56bca9caf7af5d47b171c81f5927b
Hiromi Motodera <moai.motodera@gmail.com>
2021-11-15 05:53:21 +0000

    Add basic training quiz files

--- Sources/Training/Training.swift
+++ Sources/Training/Training.swift
@@ -1,6 +1,115 @@
-public struct Training {
-    public private(set) var text = "Hello, World!"
 
-    public init() {
+public protocol TrainingItemConvertible {
+    var question: String { get }
+    associatedtype Input
+    associatedtype Output
+    func solve(_ input: Input) -> Output
+}
+
+public class Quiz000: TrainingItemConvertible {
+
+    public var question: String {
+        """
+        ---
+        実行するとHello World!と表示するプログラムを作成せよ。
+        【実行例】
+        Hello World!
+        ---
+        """
+    }
+
+    public typealias Input = Void
+    public typealias Output = String
+
+    public func solve(_ input: Input) -> Output {
+        print(question)
+        return "Hello World!"
+    }
+}
+
+public class Quiz001: TrainingItemConvertible {
+
+    public var question: String {
+        """
+        ---
+        12345+23456を計算して結果を表示するプログラムを作成せよ。
+        【実行例】
+        12345 + 23456 = 35801
+        ---
+        """
+    }
+
+    public typealias Input = (first: Int, second: Int)
+    public typealias Output = Int
+    
+    public func solve(_ input: Input) -> Output {
+        print(question)
+        return input.first + input.second
+    }
+}
+
+public class Quiz002: TrainingItemConvertible {
+
+    public var question: String {
+        """
+        12345を7で割った余りを表示するプログラムを作成せよ。
+        【実行例】
+        12345 ÷ 7 の余りは 4
+        $
+        """
+    }
+
+    public typealias Input = (number: Int, divisor: Int)
+    public typealias Output = String
+    
+    public func solve(_ input: Input) -> Output {
+        print(question)
+        let answer = input.number % input.divisor
+        return "\(input.number) ÷ \(input.divisor) の余りは \(answer)"
+    }
+}
+
+public class Quiz003: TrainingItemConvertible {
+
+    public var question: String {
+        """
+        ---
+        整数値を入力させ、その入力値を表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 123
+        your number is 123
+        ---
+        """
+    }
+
+    public typealias Input = Int
+    public typealias Output = String
+
+    public func solve(_ input: Input) -> Output {
+        print(question)
+        return "your number is \(input)"
+    }
+}
+
+public class Quiz004: TrainingItemConvertible {
+
+    public var question: String {
+        """
+        ---
+        整数値を入力させ、その入力値を3倍した計算結果を表示するプログラムを作成せよ。
+        【実行例、下線部は入力例】
+        input number: 123
+        answer = 369
+        ---
+        """
+    }
+
+    public typealias Input = Int
+    public typealias Output = String
+
+    public func solve(_ input: Input) -> Output {
+        print(question)
+        let answer = input * 3
+        return "answer = \(answer)"
     }
 }
--- Tests/TrainingTests/TrainingTests.swift
+++ Tests/TrainingTests/TrainingTests.swift
@@ -2,10 +2,24 @@ import XCTest
 @testable import Training
 
 final class TrainingTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Training().text, "Hello, World!")
+
+    func testQuiz000() throws {
+        XCTAssertEqual(Quiz000().solve(()), "Hello World!")
+    }
+
+    func testQuiz001() throws {
+        XCTAssertEqual(Quiz001().solve((12345, 23456)), 35801)
+    }
+
+    func testQuiz002() throws {
+        XCTAssertEqual(Quiz002().solve((12345, 7)), "12345 ÷ 7 の余りは 4")
+    }
+
+    func testQuiz003() throws {
+        XCTAssertEqual(Quiz003().solve(123), "your number is 123")
+    }
+
+    func testQuiz004() throws {
+        XCTAssertEqual(Quiz004().solve(123), "answer = 369")
     }
 }
